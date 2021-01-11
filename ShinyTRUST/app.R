c2e612c22a0f3c93650474af415fb886750b55be
pem725 <pem725@gmail.com>
2021-01-11 12:20:21 -0500

    update latest shinyapps

--- ShinyTRUST/app.R
+++ ShinyTRUST/app.R
@@ -8,6 +8,8 @@
 #
 
 library(shiny)
+library(ggplot2)
+library(gridExtra)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -49,36 +51,34 @@ ui <- fluidPage(
           tabPanel("Model Curves",plotOutput("p1")),
           tabPanel("Vignette Curves",plotOutput("p2")),
           tabPanel("Interaction Plot",plotOutput("p3")),
-          tabPanel("Measure by Vignette",plotOutput("p4"))
+          tabPanel("Measure by Vignette",plotOutput("p4")),
+          tabPanel("All Plots",plotOutput("p5"))
+                   )
       )
    )
-))
+)
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-   library(ggplot2)
-   library(gridExtra)
-   output$Plots <- renderPlot({
-     dat <- read.csv("AllTrustShiny.csv")
-     dat <- dat[dat$source %in% input$source & dat$study %in% input$study,]
-     x <- dat[sample(1:nrow(dat),round((input$Nperc/100)*nrow(dat))),] # random sample 
-     model <- lm(T~G:U1:R,data=x)
-     x.l <- reshape(x,varying=names(x[c(3:6,9)]),new.row.names=1:(nrow(x)*5),v.names="value",timevar="Measure",times=c("G","U1","U2","R","U3"),idvar=c("id","scen"),direction="long")
-     dat.hat <- data.frame(G=c(9,9,2,2,9,9,2,2),R=c(9,2,9,2,9,2,9,2),U1=c(9,9,9,9,2,2,2,2))
-     lm.hat <- predict(model,newdata = dat.hat,se.fit=T)
-     dat.hat <- cbind(dat.hat,lm.hat$fit,lm.hat$se.fit)
-     dat.hat$U1 <- factor(dat.hat$U1,labels=c("L","H"))
-     #print(summary(model))
-     p1 <- ggplot(x.l,aes(x=T,y=value,colour=Measure)) + geom_smooth()
-     p2 <- ggplot(x.l,aes(x=T,y=value,colour=Measure)) + geom_smooth() + facet_wrap(~scen)
-     dat.hat$G <- factor(dat.hat$G,labels=c("Low","High"))
-     dat.hat$R <- factor(dat.hat$R,labels=c("Low","High"))
-     names(dat.hat) <- c("G","R","U","T","T.se")
-     Unc.labels <- c(L = "Low Uncertainty",H="High Uncertainty")
-     p3 <- ggplot(dat.hat,aes(x=factor(G),y=T,colour=factor(R), ymin=T-2*T.se, ymax=T+2*T.se)) + geom_point(size=2) + facet_wrap(~U, labeller = labeller(U=Unc.labels)) + guides(col=guide_legend(title="Reliance")) + xlab("Goal Importance") + ylab("Overall Trust Rating (0-10)") + ylim(c(0,10)) + geom_errorbar(width=.2,col="black")
-     p4 <- ggplot(x.l,aes(x=as.factor(scen),y=value,colour=Measure))+geom_boxplot() + xlab("Scenario") + ylab("Level (0-10)")
-     grid.arrange(p1, p2, p3, p4)
-   })
+server <- function(input, output){
+   dat <- read.csv("AllTrustShiny.csv")
+   dat <- dat[dat$source %in% input$source & dat$study %in% input$study,]
+   x <- dat[sample(1:nrow(dat),round((input$Nperc/100)*nrow(dat))),] # random sample 
+   model <- lm(T~G:U1:R,data=x)
+   x.l <- reshape(x,varying=names(x[c(3:6,9)]),new.row.names=1:(nrow(x)*5),v.names="value",timevar="Measure",times=c("G","U1","U2","R","U3"),idvar=c("id","scen"),direction="long")
+   dat.hat <- data.frame(G=c(9,9,2,2,9,9,2,2),R=c(9,2,9,2,9,2,9,2),U1=c(9,9,9,9,2,2,2,2))
+   lm.hat <- predict(model,newdata = dat.hat,se.fit=T)
+   dat.hat <- cbind(dat.hat,lm.hat$fit,lm.hat$se.fit)
+   dat.hat$U1 <- factor(dat.hat$U1,labels=c("L","H"))
+   #print(summary(model))
+   output$p1 <- ggplot(x.l,aes(x=T,y=value,colour=Measure)) + geom_smooth()
+   output$p2 <- ggplot(x.l,aes(x=T,y=value,colour=Measure)) + geom_smooth() + facet_wrap(~scen)
+   dat.hat$G <- factor(dat.hat$G,labels=c("Low","High"))
+   dat.hat$R <- factor(dat.hat$R,labels=c("Low","High"))
+   names(dat.hat) <- c("G","R","U","T","T.se")
+   Unc.labels <- c(L = "Low Uncertainty",H="High Uncertainty")
+   output$p3 <- ggplot(dat.hat,aes(x=factor(G),y=T,colour=factor(R), ymin=T-2*T.se, ymax=T+2*T.se)) + geom_point(size=2) + facet_wrap(~U, labeller = labeller(U=Unc.labels)) + guides(col=guide_legend(title="Reliance")) + xlab("Goal Importance") + ylab("Overall Trust Rating (0-10)") + ylim(c(0,10)) + geom_errorbar(width=.2,col="black")
+   output$p4 <- ggplot(x.l,aes(x=as.factor(scen),y=value,colour=Measure))+geom_boxplot() + xlab("Scenario") + ylab("Level (0-10)")
+   output$p5 <- grid.arrange(p1, p2, p3, p4)
 }
 
 # Run the application 
--- plotDists/server.R
+++ plotDists/server.R
@@ -1,57 +1,73 @@
  shinyServer(
    function(input,output){
+     library(ggplot2)
      output$myPlot <- renderPlot({
        distType <- input$Distribution
        size <- as.numeric(input$sampleSize)
        if(distType=="Beta"){
-         randVec <- rbeta(size,shape1=as.numeric(input$a),shape2=as.numeric(input$b),ncp=as.numeric(input$ncp))
+         randVec <- data.frame(x=rbeta(size,shape1=as.numeric(input$a),shape2=as.numeric(input$b),ncp=as.numeric(input$ncp)))
+         plot(ggplot(randVec,aes(x=x)) + geom_histogram(aes(y = ..density..), binwidth=density(randVec$x)$bw) + geom_density(fill="blue", alpha=0.3) + theme_bw() + xlab('') + ylab('') + xlim(c(0,1)))
        } 
        if(distType=="Binomial"){
          randVec <- rbinom(size,size=as.numeric(input$trials),prob=as.numeric(input$prob))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        } 
        if(distType=="Cauchy"){
          randVec <- rcauchy(size,location=as.numeric(input$location),scale=as.numeric(input$scale))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        } 
        if(distType=="ChiSquare"){
          randVec <- rchisq(size,df=as.numeric(input$df),ncp=as.numeric(input$ncp))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        } 
        if(distType=="Exponential"){
          randVec <- rexp(size,rate=1/as.numeric(input$lambda))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        } 
        if(distType=="F"){
          randVec <- rf(size,df1=as.numeric(input$df1),df2=as.numeric(input$df2),ncp=as.numeric(input$ncp))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        } 
        if(distType=="Gamma"){
          randVec <- rgamma(size,shape=as.numeric(input$shape),scale=as.numeric(input$scale))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Geometric"){
          randVec <- rgeom(size,prob=as.numeric(input$prob))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Hypergeometric"){
          randVec <- rhyper(size,m=as.numeric(input$m),n=as.numeric(input$n),k=as.numeric(input$k))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Log-Normal"){
          randVec <- rlnorm(size,meanlog=as.numeric(input$meanlog),sdlog=as.numeric(input$sdlog))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Negative Binomial"){
          randVec <- rnbinom(size,size=as.numeric(input$size),prob=as.numeric(input$prob))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Normal"){
          randVec <- rnorm(size,mean=as.numeric(input$mean),sd=as.numeric(input$sd))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Poisson"){
          randVec <- rpois(size,lambda=as.numeric(input$lambda))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="t"){
          randVec <- rt(size,df=as.numeric(input$df),ncp=as.numeric(input$ncp))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Uniform"){
          randVec <- runif(size,min=as.numeric(input$min),max=as.numeric(input$max))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
        if(distType=="Weibull"){
          randVec <- rweibull(size,shape=as.numeric(input$shape),scale=as.numeric(input$scale))
+         hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
        }
-       hist(randVec,col="gray",main=paste(distType,"Distribution"),xlab="Random Values")
      })
    }
 )
